7dafaf75177ca5f7d918d2872940a7cfe7e796df
Xlayton <weightequalsmasstimesgravity@gmail.com>
2021-01-12 18:56:16 -0700

    Basic SPA setup

--- movie-react-front/src/App.js
+++ movie-react-front/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import React from 'react';
+import Homepage from './Homepage';
+import TestPage from './TestPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/testpage">Test Page</Link>
+          </nav>
+          <section className="content">
+            <Route exact path="/" component={Homepage}/>
+            <Route exact path="/testpage" component={TestPage}/>
+          </section>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ movie-react-front/src/TestPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class TestPage extends React.Component {
+
+    render() {
+        return (
+            <h1>Test Page</h1>
+        )
+    }
+}
