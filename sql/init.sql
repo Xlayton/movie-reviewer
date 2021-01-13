5b2b8d5b5079985dc8aaebad60f4a72b2699eb18
Xlayton <weightequalsmasstimesgravity@gmail.com>
2021-01-12 18:26:52 -0700

    Update git ignore. Update init.sql to include reviews

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,5 @@
 create database if not exists moviereviewer;
 use moviereviewer;
-create table if not exists users(id int auto_increment primary key, fname varchar(100), lname varchar(100), street varchar(100), city varchar(100), state varchar(2), zip_code varchar(5), email varchar(100), password varchar(100), phone varchar(50));
+create table if not exists users(id int auto_increment primary key, fname varchar(100) not null, lname varchar(100) not null, street varchar(100) not null, city varchar(100) not null, state varchar(2) not null, zip_code varchar(5) not null, email varchar(100) not null, password varchar(100) not null, phone varchar(50) not null);
+create table if not exists reviews(id int auto_increment primary key, user_id int not null, review_body varchar(250), movie_rating int unsigned check (movie_rating < 6), foreign key (user_id) references users(id) on delete cascade)
+
