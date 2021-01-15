298b64d5e370766e57184c57b3f9c7cb41cefde5
WistyDagel <chrisdazley@gmail.com>
2021-01-14 19:56:26 -0700

    Update Homepage.js

--- movie-react-front/src/Homepage.js
+++ movie-react-front/src/Homepage.js
@@ -9,6 +9,7 @@ export default class Homepage extends React.Component {
             password: '',
             review: '',
             rating: 0,
+            movie: '',
             renderReview: false
         }
 
@@ -17,6 +18,16 @@ export default class Homepage extends React.Component {
         this.handlePassword = this.handlePassword.bind(this);
         this.handleRating = this.handleRating.bind(this);
         this.handleReview = this.handleReview.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/550?api_key=77c34d76c76368a57135c21fcb3db278')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                movie: data
+            })
+        })
     }
 
     handleEmail = evt => {
@@ -66,23 +77,21 @@ export default class Homepage extends React.Component {
         if(this.state.renderReview){
             return (
                 <div>
-                    <form onSubmit={()=> this.postReview}>
-                        <label>Rating: </label>
-                        <select value={this.state.rating} onChange={this.handleRating}>
-                            <option value="1">1 Star</option>
-                            <option value="2">2 Stars</option>
-                            <option value="3">3 Stars</option>
-                            <option value="4">4 Stars</option>
-                            <option value="4">5 Stars</option>
-                        </select>                       
-                        <br/>
-                        <br/>
-                        <label>Review: </label>
-                        <textarea rows="10" cols="100" value={this.state.review} onChange={this.handleReview} />
-                        <br/>
-                        <br/>
-                        <input type="submit" value="Submit"/>
-                    </form>
+                    <label>Rating: </label>
+                    <select value={this.state.rating} onChange={this.handleRating}>
+                        <option value="1">1 Star</option>
+                        <option value="2">2 Stars</option>
+                        <option value="3">3 Stars</option>
+                        <option value="4">4 Stars</option>
+                        <option value="4">5 Stars</option>
+                    </select>                       
+                    <br/>
+                    <br/>
+                    <label>Review: </label>
+                    <textarea rows="10" cols="100" value={this.state.review} onChange={this.handleReview} />
+                    <br/>
+                    <br/>
+                    <button onClick={this.postReview}>Submit</button>
                 </div>
             )
         } else {
@@ -99,18 +108,23 @@ export default class Homepage extends React.Component {
                 <h1>Homepage</h1>
                 <br/>
                 <br/>
-                {/* <form onSubmit={()=> this.authenticateUser}> */}
-                    <label>Email: </label>
-                    <input type="text" value={this.state.email} onChange={this.handleEmail} />
-                    <br/>
-                    <br/>
-                    <label>Password: </label>
-                    <input type="password" value={this.state.password} onChange={this.handlePassword} />
-                    <br/>
-                    <br/>
-                    {/* <input type="submit" value="Submit"/> */}
-                    <button onClick={this.authenticateUser}>Submit</button>
-                {/* </form> */}
+                <img style={{width: 200, height: "auto"}} src={`https://image.tmdb.org/t/p/w500/${this.state.movie.poster_path}`}></img>
+                <h2>{this.state.movie.original_title}</h2>
+                <p>john.preston@tbeatty.com</p>
+                <p>ImwH@qxz56t9</p>
+                <label>Email: </label>
+                <input type="text" value={this.state.email} onChange={this.handleEmail} />
+                <br/>
+                <br/>
+                <label>Password: </label>
+                <input type="password" value={this.state.password} onChange={this.handlePassword} />
+                <br/>
+                <br/>
+                {/* <input type="submit" value="Submit"/> */}
+                <button onClick={this.authenticateUser}>Submit</button>
+                <br/>
+                <br/>
+                {this.renderRatingReview()}
             </div>
         )
     }
