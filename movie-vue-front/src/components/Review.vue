1672131ddcbc21897c13af6474fc302cb84817c8
Xlayton <weightequalsmasstimesgravity@gmail.com>
2021-01-28 18:16:01 -0700

    Pre-existing Reviews now show up

---
+++ movie-vue-front/src/components/Review.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="review">
+    <button v-if="isEditable">Edit Review</button>
+    <h1>{{user_id}}</h1>
+    <h3>{{rating}}</h3>
+    <textarea v-if="enableEdit" rows="10" cols="100" v-bind:value="review_body" v-on:change="(evt) => this.review_body = evt.target.value" />
+    <h2 v-else>{{review_body}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "review",
+  props: [
+    "editReview",
+    "index",
+    "handleRating",
+    "handleReview",
+    "refreshReviews",
+    "isEditable",
+    "review_body",
+    "rating",
+    "user_id",
+    "review_id",
+  ],
+  data() {
+      return {
+          enableEdit: false
+      }
+  }
+};
+</script>
+
+<style>
+</style>
--- movie-vue-front/src/main.js
+++ movie-vue-front/src/main.js
@@ -2,16 +2,24 @@ import Vue from 'vue';
 import App from './App.vue';
 import Router from 'vue-router';
 import Homepage from './components/Homepage.vue';
+import MovieView from './components/MovieView.vue';
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
   routes: [{
-    path: "/",
-    name: "homepage",
-    component: Homepage
-  }]
+      path: "/",
+      name: "homepage",
+      component: Homepage
+    },
+    {
+      path: "/movie",
+      name: "movieview",
+      component: MovieView,
+      props: route => ({id: route.query.id, user_id: route.query.user_id})
+    }
+  ]
 })
 
 new Vue({
