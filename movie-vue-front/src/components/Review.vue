4992e318114b15db1027fc6d08435a8cf3fffd31
Xlayton <weightequalsmasstimesgravity@gmail.com>
2021-01-28 18:41:37 -0700

    Review stars functionality

--- movie-vue-front/src/components/Review.vue
+++ movie-vue-front/src/components/Review.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="review">
-    <button v-if="isEditable">Edit Review</button>
+    <button v-if="isEditable && !enableEdit">Edit Review</button>
     <h1>{{user_id}}</h1>
-    <h3>{{rating}}</h3>
+    <ReviewStars v-bind:score="rating" :size="50" v-bind:index="index" v-bind:enableEdit="enableEdit" />
     <textarea v-if="enableEdit" rows="10" cols="100" v-bind:value="review_body" v-on:change="(evt) => this.review_body = evt.target.value" />
     <h2 v-else>{{review_body}}</h2>
+    <button v-if="enableEdit">Update</button>
   </div>
 </template>
 
 <script>
+import ReviewStars from './ReviewStars.vue';
 export default {
+  components: { ReviewStars },
   name: "review",
   props: [
     "editReview",
---
+++ movie-vue-front/src/components/ReviewStars.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="ReviewStars" v-if="!enableEdit">
+    <img
+      v-for="index in 5"
+      v-bind:key="index"
+      v-bind:src="
+        index <= score ? getImgUrl('fullStar.png') : getImgUrl('emptyStar.png')
+      "
+      v-bind:width="size"
+    />
+  </div>
+  <div class="ReviewStars" v-else>
+    <img
+      v-for="index in 5"
+      v-bind:key="index"
+      v-bind:src="
+        index <= editScore ? getImgUrl('fullStar.png') : getImgUrl('emptyStar.png')
+      "
+      v-bind:width="size"
+      v-on:click="() => setScore(index)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "reviewstars",
+  props: ["score", "size", "index", "enableEdit"],
+  data() {
+    return {
+      editScore: 0,
+    };
+  },
+  methods: {
+    getImgUrl(pic) {
+      return require("../assets/" + pic);
+    },
+    setScore(score) {
+      this.editScore = score;
+      console.log(score)
+    },
+  },
+  created() {
+      this.editScore = this.score;
+  }
+};
+</script>
+
+<style>
+</style>
