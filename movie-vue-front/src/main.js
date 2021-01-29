45b011f6169edd541849fc60b349b578eb752446
WistyDagel <chrisdazley@gmail.com>
2021-01-28 18:46:15 -0700

    Registration page has been constructed

--- movie-vue-front/src/main.js
+++ movie-vue-front/src/main.js
@@ -2,16 +2,24 @@ import Vue from 'vue';
 import App from './App.vue';
 import Router from 'vue-router';
 import Homepage from './components/Homepage.vue';
+import Register from './components/Register.vue';
 
 Vue.use(Router);
 
 const router = new Router({
   mode: 'history',
-  routes: [{
-    path: "/",
-    name: "homepage",
-    component: Homepage
-  }]
+  routes: [
+    {
+      path: "/",
+      name: "homepage",
+      component: Homepage
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    }
+  ]
 })
 
 new Vue({
